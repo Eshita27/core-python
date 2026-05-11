d0ad58495a01a4be0d10f8211cd1034a02ee3b35
Eshita Mukherjee <eshitachakraborty@outlook.com>
2026-05-11 21:50:42 +0530

    Adding different tests

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -1,4 +1,10 @@
-from src.basics.calculator import add
+from src.basics.calculator import add,subtract
 
 def test_add():
     assert add(4,7) == 11
+
+def test_subtract():
+    assert subtract(9,2) == 7
+
+def test_add_negative_numbers():
+    assert add(-4,-2) == -6
