e7c36767738778b0fa94e7b050cdf0f55f814369
Eshita Mukherjee <eshitachakraborty@outlook.com>
2026-05-11 22:05:54 +0530

    Add calculator operations and pytest improvements

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -1,10 +1,33 @@
-from src.basics.calculator import add,subtract
+import pytest
+from src.basics.calculator import add,subtract,multiply,divide
 
-def test_add():
-    assert add(4,7) == 11
+@pytest.mark.parametrize(
+    "a,b,expected",
+    [
+        (2, 3, 5),
+        (-2, -3, -5),
+        (10, 0, 10)
+    ]
+)
+
+def test_add(a,b,expected):
+    assert add(a,b) == expected
 
 def test_subtract():
     assert subtract(9,2) == 7
 
 def test_add_negative_numbers():
     assert add(-4,-2) == -6
+
+def test_divide():
+    assert divide(10,2) == 5
+
+def test_divide_by_zero():
+    with pytest.raises(ValueError):
+        divide(10,0)
+
+def test_multiply():
+    assert multiply(2,3) == 6
+
+def test_multiply_by_zero():
+        multiply(9,0)
