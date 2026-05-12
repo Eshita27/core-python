da708967bcb68bdfdfc62a4dc26dbe682b8c1196
Eshita Mukherjee <eshitachakraborty@outlook.com>
2026-05-12 21:15:30 +0530

    Add pytest fixture for student grades and refactor test to use it

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,3 +6,11 @@ def sample_numbers():
     print("\n Setting up sample numbers")
     yield 10, 5
     print("\n Cleaning up sample numbers")
+
+@pytest.fixture()
+def students():
+    return {
+        "Eshita":"A",
+        "Rahul":"B",
+        "Priya":"A+"
+    }
--- tests/test_student_utils.py
+++ tests/test_student_utils.py
@@ -8,11 +8,5 @@ from src.basics.student_utils import get_student_grade
         ("Priya", "A+")
     ])
 
-def test_get_student_grade(name, expected):
-    students = {
-        "Eshita":"A",
-        "Rahul":"B",
-        "Priya":"A+"
-    }
-
-    assert get_student_grade(students,name) == expected
+def test_get_student_grade_from_fixtures(students, name, expected):
+    assert get_student_grade(students, name) == expected
